3d7e60c7737493488224464c0bbb7a6ac227b62f
jzh04 <w2027@e-mirim.hs.kr>
2021-04-06 20:33:17 +0900

    Complete Simple Calculator

--- app/src/main/java/kr/hs/emirim/w2027/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/project4_1/MainActivity.java
@@ -3,6 +3,8 @@ package kr.hs.emirim.w2027.project4_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,16 +17,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPlus = findViewById(R.id.bnt1);
-        Button btnMinus = findViewById(R.id.bnt2);
-        Button btnMulti = findViewById(R.id.bnt3);
-        Button btnDiv = findViewById(R.id.bnt4);
+        Button btnPlus = findViewById(R.id.btn1);
+        Button btnMinus = findViewById(R.id.btn2);
+        Button btnMulti = findViewById(R.id.btn3);
+        Button btnDiv = findViewById(R.id.btn4);
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
         textResult = findViewById(R.id.t1);
-//        btnPlus.setOnTouchListener(btnListener);
-//        btnMinus.setOnTouchListener(btnListener);
-//        btnMinus.setOnTouchListener(btnListener);
-//        btnDiv.setOnTouchListener(btnListener);
+
+        btnPlus.setOnTouchListener(btnListener);
+        btnMinus.setOnTouchListener(btnListener);
+        btnMulti.setOnTouchListener(btnListener);
+        btnDiv.setOnTouchListener(btnListener);
     }
+
+    View.OnTouchListener btnListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            String strEdit1 = edit1.getText().toString();
+            String strEdit2 = edit2.getText().toString();
+            int num1 = Integer.parseInt(strEdit1);
+            int num2 = Integer.parseInt(strEdit2);
+            int result = 0;
+            switch (v.getId()){
+                case R.id.btn1:
+                    result=num1+num2;
+                    break;
+                case R.id.btn2:
+                    result=num1-num2;
+                    break;
+                case R.id.btn3:
+                    result=num1*num2;
+                    break;
+                case R.id.btn4:
+                    result=num1/num2;
+                    break;
+            }
+            textResult.setText(R.string.t1);
+            textResult.append(result+"");
+            return false;
+        }
+    };
 }
