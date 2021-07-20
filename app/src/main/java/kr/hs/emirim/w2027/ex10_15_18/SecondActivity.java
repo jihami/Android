31ffdc82f755548eb3ca4bcbc92b236b166bf10a
jzh04 <w2027@e-mirim.hs.kr>
2021-07-20 11:44:07 +0900

    Edit SecondActivity commit

---
+++ app/src/main/java/kr/hs/emirim/w2027/ex10_15_18/SecondActivity.java
@@ -0,0 +1,37 @@
+package kr.hs.emirim.w2027.ex10_15_18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import javax.xml.transform.Result;
+
+public class SecondActivity extends AppCompatActivity {
+    int sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int num1 = intent.getIntExtra("num1",0);
+        int num2 = intent.getIntExtra("num2",0);
+        sum = num1 + num2;
+
+        Button btnback = findViewById(R.id.btn_back);
+        btnback.setOnClickListener(btnListner);
+    }
+    View.OnClickListener btnListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+              Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+              mainIntent.putExtra("sum", sum);
+              setResult(RESULT_OK, mainIntent);
+              finish();
+        }
+    };
+}
