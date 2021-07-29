adf0f3a9b84a4d292a6a142a7cd38c92fc79677b
jzh04 <w2027@e-mirim.hs.kr>
2021-07-29 14:30:26 +0900

    init

--- app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
@@ -1,18 +1,7 @@
 package kr.hs.emirim.w2027.studymanager;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
