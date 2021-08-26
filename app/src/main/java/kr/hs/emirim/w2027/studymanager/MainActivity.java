26b3da76769ea6d89dae5691dba3219a11e64d4a
jzh04 <w2027@e-mirim.hs.kr>
2021-08-26 20:35:49 +0900

    오늘한 내용 Commit

--- app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.hs.emirim.w2027.studymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_main);
+//        Intent intent = new Intent(this, Second.class);
+//        startActivity(intent);
+
+
 
     }
 }
