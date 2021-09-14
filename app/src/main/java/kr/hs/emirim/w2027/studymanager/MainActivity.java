879578b23153133a37cf3fc93f53417676e6bac1
jzh04 <w2027@e-mirim.hs.kr>
2021-09-14 19:25:42 +0900

    add image,

--- app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/studymanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 
@@ -15,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnL = findViewById(R.id.btn_left);
-        Button btnR = findViewById(R.id.btn_right);
+        ImageButton btnL = findViewById(R.id.btn_left);
+        ImageButton btnR = findViewById(R.id.btn_right);
         btnL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
