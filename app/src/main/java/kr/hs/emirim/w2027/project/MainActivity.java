3360b4d7b1b6822efea38ad8a21918e1996774d3
jihami <w2027@e-mirim.hs.kr>
2021-09-21 16:53:45 +0900

    페이지 넘기기

--- app/src/main/java/kr/hs/emirim/w2027/project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/project/MainActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.w2027.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_todo = findViewById(R.id.todo);
+        btn_todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"ToDo페이지 입니다.",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Second.class);
+                startActivity(intent);
+            }
+        });
     }
 }
