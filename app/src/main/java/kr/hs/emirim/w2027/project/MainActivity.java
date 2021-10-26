dbe9ff40cbc59d0ec299a5afa83bd438f9e534b8
jihami <w2027@e-mirim.hs.kr>
2021-10-26 20:06:04 +0900

    commit

--- app/src/main/java/kr/hs/emirim/w2027/project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2027/project/MainActivity.java
@@ -18,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         Button btn_timer = findViewById(R.id.timer);
         Button btn_stopwatch = findViewById(R.id.stopWatch);
         Button btn_note = findViewById(R.id.note);
+        btn_todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Timer page",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), ToDo_1.class);
+                startActivity(intent);
+            }
+        });
         btn_timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
